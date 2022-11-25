2d7954a4edf9cc891f6dffddfef5d14616ca2521
Eduardo-RFarias <eduardo.rfarias@outlook.com>
2022-11-25 11:36:41 -0300

    Adicionado arquivos do projeto spring

---
+++ sigma_api/src/main/java/br/unb/sigma/SigmaApplication.java
@@ -0,0 +1,11 @@
+package br.unb.sigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigmaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SigmaApplication.class, args);
+	}
+}
---
+++ sigma_api/src/test/java/br/unb/sigma/SigmaApplicationTests.java
@@ -0,0 +1,15 @@
+package br.unb.sigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class SigmaApplicationTests {
+	@Test
+	void contextLoads() {
+
+	}
+}
