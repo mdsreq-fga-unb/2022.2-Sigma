0686c0b9ca07e03bae25c06a61fea1ae019ff6a2
MaltaLiz <lizlrmalta@gmail.com>
2022-11-29 19:59:27 -0300

    adicionando o normalize.css ao projeto

--- sigma-app/src/index.tsx
+++ sigma-app/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import 'normalize.css'
 import App from './AppRoutes'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
